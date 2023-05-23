b2cf6e9cbad261a229cc8884826e432e7eef3159
sami <problemot8@gmail.com>
2023-05-23 11:15:41 +0200

    add quote

---
+++ functions.py
@@ -0,0 +1,28 @@
+import random
+
+def load_quotes(filename):
+    quotes = []
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    for line in lines:
+        line = line.strip()
+        if line:
+            quotes.append(line)
+    return quotes
+
+def random_quote(quotes):
+    random_quote = random.choice(quotes)
+    return random_quote
+
+def print_quote(quote):
+    print(quote)
+
+def view_quotes(quotes):
+    for quote in quotes:
+        print_quote(quote)
+def add_quote(quotes, filename):
+    new_quote = input("Enter a new quote: ")
+    quotes.append(new_quote)
+
+    with open(filename, 'a') as file:
+        file.write(new_quote)
