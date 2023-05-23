c0ec7e4b03696adc7e95f54db4a011c33f5a3a90
Thierry <thierryhuynh3@gmail.com>
2023-05-23 11:45:06 +0200

    update main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+from functions import *
+
+def menu():
+    print("\n==== Programming Quotes ====")
+    print("1. Random quote")
+    print("2. All quotes")
+    print("3. Exit")
+    print("5. Display quotes")
+
+def main():
+    while True:
+        quotes = load_quotes("quotes.txt")
+        menu()
+
+        choice = input("Choose your an action (1-5): ")
+        
+        if choice == "1":
+            print_quote(random_quote(quotes))
+        elif choice == "2":
+            view_quotes(quotes)
+        elif choice == "3":
+            print("Good bye...")
+            break
+        elif choice == "5":
+            count = int(input("Enter the number of quotes to display: "))
+            display_quotes(quotes, count)
+        else:
+            print("Invalid input")
+
+if __name__ == "__main__":
+    main()
